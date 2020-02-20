7ad4717dbb2923fab3eb4565f277808eaf53b4d8
Swapnil Gaikwad <swapnilmg18@gmail.com>
2020-02-20 01:23:39 -0800

    basic homepage created

---
+++ src/components/header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default () => (
+    <header>
+        <div className="hero-text-box">
+            <h1>Swapnil Gaikwad</h1>
+            <h3>Web and Software Developer</h3>
+            <a className="btn btn-full" href="mailto:swapnilmg18@gmail.com">Get in touch</a>
+        </div>
+    </header>
+)
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,8 @@
 import React from "react"
+import Header from "../components/header"
 
-export default () => <div>New version on this website is under construction. Stay Tuned!</div>
+export default () => (
+    <div className="row">
+        <Header />
+    </div>
+)
