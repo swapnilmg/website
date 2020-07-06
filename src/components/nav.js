71552203b9794a5c0c1c6bec16c86582deca3a6b
Swapnil Gaikwad <swapnilmg18@gmail.com>
2020-07-05 23:52:51 -0700

    Logo and hover added

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,9 +1,11 @@
 import React from "react"
+import logo from "../../static/logo.png"
+import "./nav.css";
 
 export default () => (
     <div className="row">
         <nav>
-            <img src="../../static/logo.png" alt="Logo" />
+            <img className="logo" src={logo} alt="Logo" />
         </nav>
     </div>
 )
